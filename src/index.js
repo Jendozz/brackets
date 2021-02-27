a19bab142e26d12b4efcd9f9154d073689cc5f19
Naumov Evgeniy <n_zhenek@mail.ru>
2021-02-27 13:41:09 +0400

    feat: solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if  (str.length % 2 != 0)
+  {
+    return false;
+  }
+let strBracketsOpenClose = bracketsConfig.map(function(element){ return element.join("");}) 
+
+
+for (let i= 0; i < strBracketsOpenClose.length; i++)
+{
+  if (str.includes(strBracketsOpenClose[i]))
+  {
+    str = str.replace(strBracketsOpenClose[i], "")
+    i = -1;
+  }
 }
+ if (str.length == 0 ) return true;
+else return false; 
+    
+}
+
+
